afc90029e5c208b14cc67e30db311836a225d2ca
lltheis <theis.laura.l@gmail.com>
2016-06-13 20:27:21 +0000

    Lesson 5.2 'Forms'

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'static_pages#landing_page'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
+  #root 'static_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
