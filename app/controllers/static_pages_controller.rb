ab48db91e1f9c10f62d4d6c364f74fc99ab447d8
lltheis <theis.laura.l@gmail.com>
2016-06-14 14:07:01 +0000

    fix db

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,15 @@ class StaticPagesController < ApplicationController
 
   def index
   end
+
+	def thank_you
+		@name = params[:name]
+		@email = params[:email]
+		@message = params[:message]
+		ActionMailer::Base.mail(:from => @email,
+			:to => 'theis.laura.l@gmail.com',
+			:subject => "A new contact form message from #{@name}",
+			:body => @message).deliver_now
+	end
+
 end
