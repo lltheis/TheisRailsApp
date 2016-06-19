a42302078d328b359d98cbf75dc5b7cb0b17c64b
lltheis <theis.laura.l@gmail.com>
2016-06-19 13:55:26 +0000

    Lesson 5.5 'Authentication'

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'static_pages/about'
   get 'static_pages/contact'
