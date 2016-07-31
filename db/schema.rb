d03cc6ffe096810d8228d55a0111037f2206ffef
lltheis <theis.laura.l@gmail.com>
2016-07-31 13:01:55 +0000

    Lesson 6.5 Payment Forms with Stripe, migration for price to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707031834) do
+ActiveRecord::Schema.define(version: 20160731125206) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -38,10 +38,10 @@ ActiveRecord::Schema.define(version: 20160707031834) do
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "colour"
-    t.integer  "price"
+    t.decimal  "price",       precision: 8, scale: 2
   end
 
   create_table "users", force: :cascade do |t|
