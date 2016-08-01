af49093e14cf35d4ecccc1af5127e9080f0d8fb9
lltheis <theis.laura.l@gmail.com>
2016-08-01 23:40:44 +0000

    Add Bootstrap class horizontal-form to products form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,17 +36,16 @@ class ProductsController < ApplicationController
 
   # POST /products
   # POST /products.json
-  def create
-    @product = Product.find(params[:product_id])
-    @comment = @product.comments.new(comment_params)
-    @comment.user = current_user
+def create
+    @product = Product.new(product_params)
+    
     respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
