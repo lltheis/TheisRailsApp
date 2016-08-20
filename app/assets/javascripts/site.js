2cfbddac954e5b52549cb018957153ff8a0e49ab
lltheis <theis.laura.l@gmail.com>
2016-08-20 00:48:32 +0000

    Testing assets path

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -16,8 +16,8 @@
 //= require_tree .
 
 $(document).on('turbolinks:load', function() {
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets',
+  $('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets/images',
 		readOnly: true,
 		score: function() {
 			return $(this).attr('data-score');
