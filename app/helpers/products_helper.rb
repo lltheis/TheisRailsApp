88d90b7946b263a51162bca717f3948ef723d360
lltheis <theis.laura.l@gmail.com>
2016-09-12 21:52:59 +0000

    add no results search re-direct

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,16 @@
 module ProductsHelper
-	def cache_key_for_products
-		count = Product.count
-		max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
-		"products/all-#{count}-#{max_updated_at}"
-	end
+  def show_products(products)
+    if products.length > 0
+      render partial: products
+    else
+      ("<div class='card' role='alert'><h4>Sorry! No products were found. Please search again.</h4></div>").html_safe
+    end
+  end
+
+  def cache_key_for_products
+    count          = Product.count
+    max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "products/all-#{count}-#{max_updated_at}"
+  end
 end
+
